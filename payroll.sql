dcd9d0675dad8fd0838ed688c9b5ef0b509f0643
payalbanode0 <payalbanode0@gmail.com>
2022-04-02 00:05:38 +0530

    added alter table

---
+++ payroll.sql
@@ -0,0 +1,18 @@
+use payrollservices
+
+
+
+ALTER TABLE employee_payroll
+ADD Phone_Number VarChar(12)
+Go
+ALTER TABLE employee_payroll
+ADD departmen varchar(20)
+Go
+ALTER TABLE employee_payroll
+ADD emp_address varchar(20)
+Go
+
+select *
+from employee_payroll
+Go
+
